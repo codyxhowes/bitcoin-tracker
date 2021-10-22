e26877c8ed6577b36d650a90fc98d64224c6456a
Cody <howes363@gmail.com>
2021-10-21 21:25:47 -0500

    added percent change and olor animation

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-let ws = new WebSocket('wss://stream.binance.us:9443/ws/btcusd@trade');
+let ws = new WebSocket('wss://stream.binance.us:9443/ws/btcusd@ticker');
 let stockPriceElement = document.getElementById('current-btc-price');
+let stockDayPrice = document.getElementById('current-day-price')
 let lastPrice = null;
 
 ws.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
-    let price = parseFloat(stockObject.p).toFixed(2);
-    console.log(stockObject.p);
+    let price = parseFloat(stockObject.c).toFixed(2);
+    let day = parseFloat(stockObject.P).toFixed(2);
+    console.log(stockObject.c);
     stockPriceElement.innerText = price;
+    stockDayPrice.innerText = day;
 
     stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'rgb(0,255,171)': 'red';
+    stockDayPrice.style.color = !lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'rgb(0,255,171)': 'red';
 
     lastPrice = price;
 };
